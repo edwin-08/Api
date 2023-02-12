d34c4be6a06ae6423ee727f91e579c47448da9d5
edwin-08 <edwinbueno84@gmail.com>
2023-02-12 17:52:38 -0500

    cors

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ControllerUsuarios;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register_user', [ControllerUsuarios::class, 'registrar_usuario_ruta']);
+Route::group(['middleware' => ['cors']], function () {
+    Route::post('register_user', [ControllerUsuarios::class, 'registrar_usuario_ruta']);
+});
+
+
